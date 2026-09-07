9bbe282df860e9efa43c8c8e5c64ffcd84835ff8
Peter Grajcar <peter@grajcar.eu>
2026-09-07 22:28:00 +0200

    fix download script
    Signed-off-by: Peter Grajcar <peter@grajcar.eu>

--- download_models.sh
+++ download_models.sh
@@ -2,6 +2,6 @@
 set -e
 mkdir -p models
 cd models
-curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3278{/slovak-morfflex-pdt-170914.zip}
+curl -o "slovak-morfflex-pdt-170914.zip" "https://lindat.mff.cuni.cz/repository/server/api/core/bitstreams/handle/11234/1-3278/slovak-morfflex-pdt-170914.zip"
 unzip slovak-morfflex-pdt-170914.zip
 rm slovak-morfflex-pdt-170914.zip
